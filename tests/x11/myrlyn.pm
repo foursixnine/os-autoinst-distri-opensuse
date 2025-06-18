bddcef547725a11b95b479999f930a74320cb641
Santiago Zarate <santiago.zarate@suse.com>
2025-06-18 12:41:00 +0200

    Myrlyn has its own authentication method

--- tests/x11/myrlyn.pm
+++ tests/x11/myrlyn.pm
@@ -11,7 +11,7 @@ sub run {
     send_key 'alt-f4';
 
     # now start myrlyn in rw
-    x11_start_program('myrlyn-sudo', target_match => 'root-auth-dialog');
+    x11_start_program('myrlyn-sudo', target_match => 'myrlyn-askpass');
     type_password;
     send_key 'ret';
 
