0897f81837c48a43aa20c7318b640a91d1fdbd90
Santiago Zarate <santiago.zarate@suse.com>
2025-06-18 12:04:14 +0200

    Start myrlyn in root mode

--- tests/x11/myrlyn.pm
+++ tests/x11/myrlyn.pm
@@ -5,7 +5,13 @@ use testapi;
 
 sub run {
     ensure_installed('myrlyn');
+
+    #start myrlyn in read-only mode
     x11_start_program('myrlyn');
+    send_key 'alt-f4';
+
+    # now start myrlyn in rw
+    x11_start_program('xdg-su -c myrlyn');
     send_key 'alt-f4';
 }
 
